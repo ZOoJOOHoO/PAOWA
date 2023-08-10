1935b0d7bbdbc7266a272b30475c2b7de5e1cfbe
ZJH111 <2039065994@qq.com>
2023-08-10 21:21:19 +0800

    1

---
+++ src/main/java/com/example/power/utils/B.java
@@ -0,0 +1,9 @@
+package com.example.power.utils;
+// ^^ @author ZJH111
+// ^^ @date 2023/8/10
+
+public class B {
+    public static void main(String[] args) {
+        System.out.println("BBBB");
+    }
+}
