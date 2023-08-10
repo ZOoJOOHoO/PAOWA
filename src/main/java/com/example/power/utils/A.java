1c5b27e842661da37d40e25901e04c72564f0f60
ZJH111 <2039065994@qq.com>
2023-08-10 21:24:42 +0800

    1

--- src/main/java/com/example/power/utils/A.java
+++ src/main/java/com/example/power/utils/A.java
@@ -4,6 +4,6 @@ package com.example.power.utils;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("发生改变  主分支");
+        System.out.println("B分支");
     }
 }
