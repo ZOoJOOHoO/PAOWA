29aac9993447d320edfa90c284280f4090625c06
ZJH111 <2039065994@qq.com>
2023-08-10 21:39:41 +0800

    1

--- src/main/java/com/example/power/utils/A.java
+++ src/main/java/com/example/power/utils/A.java
@@ -5,7 +5,8 @@ package com.example.power.utils;
 public class A {
     public static void main(String[] args) {
         System.out.println("B分支修改主分支");
-        System.out.println("A分支修改主分支");
-
+        System.out.println("B分支修改主分支");
+        System.out.println("B分支修改主分支");
+        System.out.println("B分支修改主分支");
     }
 }
